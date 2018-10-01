b131b859d1abb1a67a7c501010ef8e1d4ad61bad
Johnny Kekker <giomail.iv@gmail.com>
2018-10-01 03:40:33 +0300

    return to good old jupyter

--- lab1/half_div.py
+++ lab1/half_div.py
@@ -1,15 +1,16 @@
-from test_env import fun, validate
+from test_env import fun, validate, interval_len
 
-def half(a, b, eps, i = 0):   
-  x_0 = (a + b)/2  
-  if a < 0 and b > 0:
-    l = b + abs(a)
-  elif a < 0 and b < 0:
-  	l = abs(a) - abs(b)
-  else: l = b - a
+def half(a, b, eps, i = 1, iter_meta = [["i", "a", "b", "x", "f(a)", "f(b)", "f(x)", "len"]]):
+  x_0 = (a + b)/2
+
+  l = interval_len(a, b)
+
+  iter_meta.append([i, a, b, x_0, fun(a), fun(b), fun(x_0), l])
+
   if abs(fun(x_0)) <= eps:
-    return [x_0, fun(x_0), i]
+    return [x_0, fun(x_0), i, iter_meta]
+
   if validate(a, x_0):
-    return half(a, x_0, eps, i + 1)
+    return half(a, x_0, eps, i + 1, iter_meta)
   else:
-    return half(x_0, b, eps, i + 1)
+    return half(x_0, b, eps, i + 1, iter_meta)
--- lab1/test_env.py
+++ lab1/test_env.py
@@ -21,12 +21,20 @@ def build_plot():
     x = np.array(np.arange(-20, 20, step=0.1))
     y = (x**3) + (4.81 * x**2) - (17.37 * x) + 5.38
     plt.plot(x, y)
-    plt.savefig('foo.png')
+    plt.show()
 
 def fun(x, coef_0 = 1, coef_1 = 4.81, coef_2 = -17.37, coef_3 = 5.38):
     return (coef_0 * x**3) + (coef_1 * x**2) + (coef_2 * x) + coef_3
 
 def validate(a, b):
-  if fun(a)*fun(b) < 0:
-    return True
-  else: return False
+    if fun(a)*fun(b) < 0:
+        return True
+    else: return False
+
+def interval_len(a, b):
+    if a < 0 and b > 0:
+        l = b + abs(a)
+    elif a < 0 and b < 0:
+        l = abs(a) - abs(b)
+    else: l = b - a
+    return l
